7d24d056ffe1a3ab6310bd9a16749ddcc3ee1a58
jgolden6 <jgolden6@my.westga.edu>
2021-11-19 09:06:55 -0500

    Changed CustomerDAL.RetrieveCustomers to use a stored procedure, and refactored the code.

--- RentMeRentalSystem/DAL/CustomerDAL.cs
+++ RentMeRentalSystem/DAL/CustomerDAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DBAccess.DAL;
 using MySql.Data.MySqlClient;
 using RentMeRentalSystem.Model;
 
@@ -12,50 +13,52 @@ namespace RentMeRentalSystem.DAL
     {
         public List<Customer> RetrieveCustomers()
         {
-            using var conn = new MySqlConnection(Connection.connectionString);
-            conn.Open();
-            var query = $"select * from customer";
-            using var cmd = new MySqlCommand(query, conn);
-            var retrieved = new List<Customer>();
-            using var reader = cmd.ExecuteReader();
-            var fnameOrdinal = reader.GetOrdinal("fname");
-            var lnameOrdinal = reader.GetOrdinal("lname");
-            var idOrindal = reader.GetOrdinal("customerId");
-            var genderOrdinal = reader.GetOrdinal("gender");
-            var birthdateOrindal = reader.GetOrdinal("birthdate");
-            var phoneNumberOrdinal = reader.GetOrdinal("phoneNumber");
-            var address1Ordinal = reader.GetOrdinal("addressLine1");
-            var address2Ordinal = reader.GetOrdinal("addressLine2");
-            var zipcodeOrdinal = reader.GetOrdinal("zipcode");
-            var cityOrdinal = reader.GetOrdinal("city");
-            var stateOrdinal = reader.GetOrdinal("state");
-            var registrationDateOrdinal = reader.GetOrdinal("registrationDate");
+            var customerList = new List<Customer>();
 
-            while (reader.Read())
+            using (var conn = new MySqlConnection(Connection.connectionString))
             {
-                var fname = !reader.IsDBNull(fnameOrdinal) ? reader.GetString(fnameOrdinal) : null;
-                var lname = !reader.IsDBNull(lnameOrdinal) ? reader.GetString(lnameOrdinal) : null;
-                var customerId = !reader.IsDBNull(idOrindal) ? reader.GetString(idOrindal) : null;
-                var gender = !reader.IsDBNull(genderOrdinal) ? Enum.Parse(typeof(Gender), reader.GetString(genderOrdinal)) : null;
-                var birthdate = !reader.IsDBNull(birthdateOrindal) ? reader.GetString(birthdateOrindal) : null;
-                var phoneNumber = !reader.IsDBNull(phoneNumberOrdinal) ? reader.GetString(phoneNumberOrdinal) : null;
-                var addressLine1 = !reader.IsDBNull(address1Ordinal) ? reader.GetString(address1Ordinal) : null;
-                var addressLine2 = !reader.IsDBNull(address2Ordinal) ? reader.GetString(address2Ordinal) : null;
-                var zipcode = !reader.IsDBNull(zipcodeOrdinal) ? reader.GetString(zipcodeOrdinal) : null;
-                var city = !reader.IsDBNull(cityOrdinal) ? reader.GetString(cityOrdinal) : null;
-                var state = !reader.IsDBNull(stateOrdinal) ? reader.GetString(stateOrdinal) : null;
-                var registrationDate = !reader.IsDBNull(registrationDateOrdinal) ? reader.GetString(registrationDateOrdinal) : null;
-
-                retrieved.Add(new Customer
+                conn.Open();
+                using (var cmd = new MySqlCommand("uspGetCustomers", conn))
                 {
-                    Fname = fname, Lname = lname, IdNumber = customerId, Gender = (Gender)gender, Birthdate = DateTime.Parse(birthdate),
-                    PhoneNumber = phoneNumber, AddressLine1 = addressLine1, AddressLine2 = addressLine2, Zipcode = zipcode,
-                    City = city, State = state, RegistrationDate = DateTime.Parse(registrationDate)
-                });
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var fname = reader.GetString(reader.GetOrdinal("fname"));
+                            var lname = reader.GetString(reader.GetOrdinal("lname"));
+                            var customerId = reader.GetString(reader.GetOrdinal("customerId"));
+                            var gender = Enum.Parse(typeof(Gender), reader.GetString(reader.GetOrdinal("gender")));
+                            var birthdate = reader.GetString(reader.GetOrdinal("birthdate"));
+                            var phoneNumber = reader.GetString(reader.GetOrdinal("phoneNumber"));
+                            var addressLine1 = reader.GetString(reader.GetOrdinal("addressLine1"));
+                            var addressLine2 = reader.GetString(reader.GetOrdinal("addressLine2"));
+                            var zipcode = reader.GetString(reader.GetOrdinal("zipcode"));
+                            var city = reader.GetString(reader.GetOrdinal("city"));
+                            var state = reader.GetString(reader.GetOrdinal("state"));
+                            var registrationDate = reader.GetString(reader.GetOrdinal("registrationDate"));
+
+                            customerList.Add(new Customer
+                            {
+                                Fname = fname,
+                                Lname = lname,
+                                IdNumber = customerId,
+                                Gender = (Gender)gender,
+                                Birthdate = DateTime.Parse(birthdate),
+                                PhoneNumber = phoneNumber,
+                                AddressLine1 = addressLine1,
+                                AddressLine2 = addressLine2,
+                                Zipcode = zipcode,
+                                City = city,
+                                State = state,
+                                RegistrationDate = DateTime.Parse(registrationDate)
+                            });
+                        }
+                    }
+                }
             }
 
-            conn.Close();
-            return retrieved;
+            return customerList;
         }
 
         public void RegisterCustomer(Customer customer)
